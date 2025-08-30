1e254857b7deddbe5a5d4394f8befd849916083d
Ananta Gopal <suprovapratyusha@gmail.com>
2025-08-30 20:58:25 +0530

    add .env and gitignore, update Verify.jsx

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -4,8 +4,8 @@ import { ShopContext } from '../context/ShopContext'
 import { useSearchParams } from 'react-router-dom'
 import { useEffect } from 'react'
 import { toast } from 'react-toastify'
-
 import axios from 'axios'
+
 const Verify = () => {
 
     const { navigate, token, setCartItems, backendUrl} = useContext(ShopContext)
